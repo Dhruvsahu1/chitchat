ba96e85c39a6916493215c0b4ac7f598764fbb0d
Dhruv <dhruvsahu7355@gmail.com>
2025-02-14 23:03:49 +0530

    User Authentication completed

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,24 @@
+
+import { Router } from 'express'
+import * as userController from '../controller/user.controller.js'
+import { body } from 'express-validator';
+import * as authMiddleware from '../middleware/auth.middleware.js'
+
+const router = Router();
+
+router.post("/register",
+    body('email').isEmail().withMessage('Email must be a valid email address'),
+    body('password').isLength({min:3}).withMessage('password must be at least of three length'),
+    userController.createUserController);
+
+router.post('/login',
+    body('email').isEmail().withMessage('Email must be a valid email'),
+    body('password').isLength({min:3 }).withMessage('password must be at least of three length'),
+    userController.loginController);
+
+router.get('/profile',authMiddleware.authUser,userController.profileController);
+
+router.get('/logout',authMiddleware.authUser,userController.logoutController);
+
+
+export default router;
